05c8258c1084236cbac521d48d81190bf910cf47
Aaron <eairdidh@protonmail.com>
2022-08-13 18:32:15 +0800

    Add canvas size changer button And minor changes for the css file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let gridSize = 16;
+
 const container = document.querySelector('#container');
 
 function createGrid() {
@@ -6,7 +8,7 @@ function createGrid() {
     grid.classList.add('grid');
 }
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < gridSize * gridSize; i++) {
     createGrid();    
 }
 
@@ -16,3 +18,20 @@ grid.forEach((grid) => {
     grid.classList.add('paint');
     });
 });
+
+const size = document.querySelector('button');
+size.onclick = () => {
+    gridSize = parseInt(prompt('Enter the grid size'));
+    container.setAttribute('style', `grid-template: repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr);`)
+    document.querySelectorAll('.grid').forEach(grid => {
+        grid.remove();
+    });
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        createGrid();    
+    } 
+    const grid = document.querySelectorAll('.grid').forEach((grid) => {
+        grid.addEventListener('mouseout', () => {
+        grid.classList.add('paint');
+        });
+    });
+}
