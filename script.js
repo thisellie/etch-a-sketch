d473676bf7ec70912a5fa007b3026d2b7b3cf834
Aaron <eairdidh@protonmail.com>
2022-08-17 15:31:11 +0800

    Add clear, eraser, random, and color buttons And added a container for all buttons as a toolbar and few changes with the html and css

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 let gridSize = 16;
 let lightness = 100;
-let counter = 1;
 
 function createCanvas() {
-    container.setAttribute('style', 
+    canvas.setAttribute('style', 
     `grid-template: repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr);`);
     for (let i = 0; i < gridSize * gridSize; i++) {
         const grid = document.createElement('div');
-        document.querySelector('#container').appendChild(grid);
+        canvas.appendChild(grid);
         grid.classList.add('grid');
-    }   document.querySelectorAll('.grid').forEach((grid) => {
-        grid.addEventListener('mouseover', () => { grid.setAttribute('style', 
-        `background-color: ${setRandomColor()}`)
-    }); grid.addEventListener('mouseout', () => { grid.setAttribute('style', 
-        `background-color: ${trailColor}`)
-    }); counter = 1; lightness = 100; });
+    } 
 } 
 
+function toggleBrush(mode) {
+    switch (mode) {
+        case 'Color':
+            document.querySelectorAll('.grid').forEach(grid => {grid.replaceWith(grid.cloneNode(true))})
+            document.querySelectorAll('.grid').forEach(grid => {grid.addEventListener('mouseover', () => {grid.setAttribute('style',`background-color: #000`)})});
+            break;
+        case 'Random':
+            document.querySelectorAll('.grid').forEach(grid => {grid.replaceWith(grid.cloneNode(true))})
+            document.querySelectorAll('.grid').forEach(grid => {grid.addEventListener('mouseover', () => {grid.setAttribute('style', `background-color: ${setRandomColor()}`)})}); 
+            break;
+        case 'Eraser':
+            document.querySelectorAll('.grid').forEach(grid => {grid.replaceWith(grid.cloneNode(true))})
+            document.querySelectorAll('.grid').forEach(grid => {grid.addEventListener('mouseover', () => {grid.removeAttribute('style')})}); 
+            break;
+    }
+}
+
 function setRandomColor() {
-    if (counter > 10) {
-        console.log(`If statement: ${counter} & ${lightness}`);
-        counter = 1; lightness = 100; 
-        trailColor = 'hsl(0, 0%, 0%)';
-        return trailColor;
+    if (lightness === 10) {
+        lightness = 100; 
+        return 'black';
     } else {
-        console.log(`${counter} & ${lightness}`);
-        trailColor = createRandomColor();
-        lightness -= 10; counter++;
-        return trailColor;
+        lightness -= 10;
+        return createRandomColor();
     }
 }
 
 function createRandomColor() {
     let hue = 1 + Math.random() * (360 - 1);
     let saturation = 0 + Math.random() * (100 - 0);
-    return `hsl(${hue.toFixed()}, ${saturation.toFixed()}%, ${lightness}%)`
+    return `hsl(${hue.toFixed()} ${saturation.toFixed()}% ${lightness}%)`
 }
 
-document.querySelector('button').onclick = () => {
+document.querySelector('#size').onclick = () => {
     gridSize = parseInt(prompt('Enter the grid size'));
     if (gridSize > 100 || gridSize < 2) {
         alert('Invalid grid size! Try again');
@@ -46,4 +53,10 @@ document.querySelector('button').onclick = () => {
         createCanvas();
     }
 
+document.querySelector('#clear').onclick = () => {document.querySelectorAll('.grid').forEach((grid) => {grid.removeAttribute('style')})}
+document.querySelector('#eraser').onclick = () => {toggleBrush('Eraser')}
+document.querySelector('#random').onclick = () => {toggleBrush('Random')}
+document.querySelector('#color').onclick = () => {toggleBrush('Color')}
+
 createCanvas();
+toggleBrush('Color');
